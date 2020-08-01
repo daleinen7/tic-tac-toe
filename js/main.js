d82129ea5c7d16795cb6a3a05554464b6a48d897
daleinen7 <daleinen@gmail.com>
2020-08-01 14:50:08 -0500

    game function displays and works

--- js/main.js
+++ js/main.js
@@ -1,20 +1,35 @@
 /*----- constants -----*/
 
 /*----- app's state (variables) -----*/
-const grid = [0, 0, -1, 0, 1, 0, 0, 0, 0 ,0];
-
+const grid = [0, 0, 0, 0, 0, 0, 0, 0, 0 ,0];
+playerXTurn = true;
 
 /*----- cached element references -----*/
 const gridEls = document.querySelectorAll('.square');
 
 /*----- event listeners -----*/
 // on click - if the square is unselected light up and select the grid square for the player
+document.getElementById('board').addEventListener('click', squareSelect);
 
 // on hover - player's token will light up if the square is not already selected
 
 /*----- functions -----*/
 // INIT
 
+function squareSelect(e) {
+    // find the square clicked and have it's ID update the corrosponding array index in the grid array
+    const index = parseInt(e.target.id.replace('square', ''));
+
+    if (playerXTurn) {
+        grid[index] = 1;
+    } else if (!playerXTurn) {
+        grid[index] = -1;
+    }
+
+    playerXTurn = playerXTurn ? false : true;
+
+    render();
+}
 
 function render() {
     // for the length of the grid array
